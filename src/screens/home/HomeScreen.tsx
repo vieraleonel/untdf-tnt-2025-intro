03ae32e99c52e43117af2248676a9b0ef78a8e76
Leonel Viera <vieraleonel1@gmail.com>
2025-04-28 18:47:53 -0300

    Probando fuentes en home screen

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet, View } from "react-native";
+import { StatusBar, StyleSheet, Text, View } from "react-native";
 import { HomeHeader } from "./componentes/HomeHeader";
 import { HomeBanner } from "./componentes/HomeBanner";
 import { CategoryList } from "./componentes/CategoryList";
 import { CoffeeList } from "./componentes/CoffeeList";
+import { TextPressStart2P } from "./componentes/TextPressStart2P";
 
 export function HomeScreen() {
   return (
     <View style={styles.screenContainer}>
+      <StatusBar backgroundColor="rgba(0,0,0,0.9)" barStyle="light-content" />
       <HomeHeader />
       <HomeBanner />
+      <TextPressStart2P>HOLA REGULAR</TextPressStart2P>
+      <TextPressStart2P style={{ fontSize: 40 }}>HOLA BOLD</TextPressStart2P>
       <View style={styles.separator} />
       <CategoryList />
       <CoffeeList />
